48cb22f9201c64538cbc0177e56ff52e0cc2bf1c
Ajharul Islam Akanda <ajhaka@icps.local>
2025-11-25 15:09:23 +0600

    First commit with skeleton 20251125

---
+++ src/main/java/com/ajhar/productservicemanagement/ProductServiceManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ajhar.productservicemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceManagementApplication.class, args);
+        System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/com/ajhar/productservicemanagement/ProductServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajhar.productservicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
