20c15ddcfe125bae831140234bcc81f4c3b0cf58
Ajhar21 <2017ajharakanda@gmail.com>
2025-11-26 01:41:04 +0600

    Fix:CommandLineRunner added to solve initial null laod for createdAt

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into products(name,description,price) values('Iphone 10', 'Super emulated display', 100000.00);
-insert into products(name,description,price) values('Macbook 5', 'Small display', 1000050.00);
-insert into products(name,description,price) values('Cricket Bat', 'With C shape', 10000.00);
-insert into products(name,description,price) values('badminton Bat', 'High pressure tolerable', 15000.00);
-insert into products(name,description,price) values('Royal Enfield', 'Perfect cafe racer', 400000.00);
